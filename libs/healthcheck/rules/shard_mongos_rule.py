9a2872f111b7a2a89d09aedbdfcec1dfe88fa099
Yaoxing <yaoxing.zhang@gmail.com>
2025-11-23 23:41:24 +0100

    + add shard mongos rule and tests.

---
+++ libs/healthcheck/rules/shard_mongos_rule.py
@@ -0,0 +1,62 @@
+from libs.healthcheck.rules.base_rule import BaseRule
+from libs.healthcheck.shared import MAX_MONGOS_PING_LATENCY, SEVERITY
+from libs.healthcheck.issues import ISSUE, ISSUE_MSG_MAP
+
+
+class ShardMongosRule(BaseRule):
+    def apply(self, data: object, result_template=None) -> object:
+        """Check the sharded cluster mongos nodes for any issues.
+
+        Args:
+            data (object): The sharded cluster status data.
+            result_template (object, optional): The template for the result. Defaults to None.
+        Returns:
+            list: A list of sharded cluster mongos check results.
+        """
+        template = result_template or {}
+        test_result = []
+        active_mongos = []
+        for mongos in data:
+            if mongos.get("pingLatencySec", 0) > MAX_MONGOS_PING_LATENCY:
+                issue_id = ISSUE.IRRESPONSIVE_MONGOS
+                test_result.append(
+                    template
+                    | {
+                        "id": issue_id,
+                        "host": mongos["host"],
+                        "severity": SEVERITY.LOW,
+                        "title": ISSUE_MSG_MAP[issue_id]["title"],
+                        "description": ISSUE_MSG_MAP[issue_id]["description"].format(
+                            host=mongos["host"],
+                            ping_latency=round(mongos["pingLatencySec"]),
+                        ),
+                    }
+                )
+            else:
+                active_mongos.append(mongos["host"])
+
+        if len(active_mongos) == 0:
+            issue_id = ISSUE.NO_ACTIVE_MONGOS
+            test_result.append(
+                template
+                | {
+                    "id": issue_id,
+                    "host": "cluster",
+                    "severity": SEVERITY.HIGH,
+                    "title": ISSUE_MSG_MAP[issue_id]["title"],
+                    "description": ISSUE_MSG_MAP[issue_id]["description"],
+                }
+            )
+        if len(active_mongos) == 1:
+            issue_id = ISSUE.SINGLE_MONGOS
+            test_result.append(
+                template
+                | {
+                    "id": issue_id,
+                    "host": "cluster",
+                    "severity": SEVERITY.HIGH,
+                    "title": ISSUE_MSG_MAP[issue_id]["title"],
+                    "description": ISSUE_MSG_MAP[issue_id]["description"].format(host=active_mongos[0]),
+                }
+            )
+        return test_result
---
+++ tests/hc/test_shard_mongos_rule.py
@@ -0,0 +1,60 @@
+from libs.healthcheck.issues import ISSUE, ISSUE_MSG_MAP
+from libs.healthcheck.rules.shard_mongos_rule import ShardMongosRule
+from libs.healthcheck.shared import SEVERITY
+from datetime import datetime, timezone
+from libs.version import Version
+
+MONGOS_IRRESPONSIVE = [
+    {
+        "host": "M-QTFH0WFXLG:30017",
+        "pingLatencySec": 0.975336,
+        "lastPing": datetime(2025, 11, 23, 22, 28, 19, 79000, tzinfo=timezone.utc),
+        "version": Version([5, 0, 19, 0]),
+    },
+    {
+        "host": "M-QTFH0WFXLG:30028",
+        "client": None,
+        "pingLatencySec": 8846940,
+        "lastPing": datetime(2025, 8, 13, 12, 59, 19, 978000, tzinfo=timezone.utc),
+    },
+]
+
+MONGOS_NO_ACTIVE = [
+    {
+        "host": "M-QTFH0WFXLG:30028",
+        "client": None,
+        "pingLatencySec": 8846940,
+        "lastPing": datetime(2025, 8, 13, 12, 59, 19, 978000, tzinfo=timezone.utc),
+    }
+]
+
+
+def test_irresponsive_mongos():
+    rule = ShardMongosRule()
+    results = rule.apply(MONGOS_IRRESPONSIVE)
+    assert len(results) == 2
+    assert results[0]["id"] == ISSUE.IRRESPONSIVE_MONGOS
+    assert results[0]["severity"] == SEVERITY.LOW
+    assert results[0]["title"] == ISSUE_MSG_MAP[ISSUE.IRRESPONSIVE_MONGOS]["title"]
+    assert results[0]["host"] == "M-QTFH0WFXLG:30028"
+
+    assert results[1]["id"] == ISSUE.SINGLE_MONGOS
+    assert results[1]["severity"] == SEVERITY.HIGH
+    assert results[1]["title"] == ISSUE_MSG_MAP[ISSUE.SINGLE_MONGOS]["title"]
+    assert results[1]["host"] == "cluster"
+
+
+def test_no_active_mongos():
+    rule = ShardMongosRule()
+    results = rule.apply(MONGOS_NO_ACTIVE)
+    assert len(results) == 2
+
+    assert results[0]["id"] == ISSUE.IRRESPONSIVE_MONGOS
+    assert results[0]["severity"] == SEVERITY.LOW
+    assert results[0]["title"] == ISSUE_MSG_MAP[ISSUE.IRRESPONSIVE_MONGOS]["title"]
+    assert results[0]["host"] == "M-QTFH0WFXLG:30028"
+
+    assert results[1]["id"] == ISSUE.NO_ACTIVE_MONGOS
+    assert results[1]["severity"] == SEVERITY.HIGH
+    assert results[1]["title"] == ISSUE_MSG_MAP[ISSUE.NO_ACTIVE_MONGOS]["title"]
+    assert results[1]["host"] == "cluster"
