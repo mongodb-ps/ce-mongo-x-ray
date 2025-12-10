4697fdfdb9b9d1c89b5abf8801d9bb71df6309e3
Yaoxing <yaoxing.zhang@gmail.com>
2025-12-10 12:36:23 +0100

    - add unit test for host info parser.

---
+++ tests/hc/parsers/test_host_info_parser.py
@@ -0,0 +1,104 @@
+from x_ray.healthcheck.parsers.host_info_parser import HostInfoParser
+
+hostInfos = [
+    {
+        "system": {
+            "hostname": "M-QTFH0WFXLG:30017",
+            "memLimitMB": 32768,
+            "numCores": 10,
+            "cpuArch": "x86_64",
+            "numaEnabled": False,
+        },
+        "os": {"type": "Darwin", "name": "Mac OS X", "version": "24.6.0"},
+        "extra": {
+            "cpuFrequencyMHz": 2400,
+            "cpuString": "Apple M1 Max",
+        },
+    },
+    {
+        "system": {
+            "hostname": "ip-172-31-22-196",
+            "memLimitMB": 7736,
+            "numCores": 2,
+            "numCpuSockets": 1,
+            "cpuArch": "x86_64",
+            "numaEnabled": False,
+        },
+        "os": {"type": "Linux", "name": "Ubuntu", "version": "22.04"},
+        "extra": {
+            "extra": {
+                "cpuString": "Intel(R) Xeon(R) Platinum 8175M CPU @ 2.50GHz",
+                "cpuFrequencyMHz": "3200.679",
+                "maxOpenFiles": 64000,
+                "mountInfo": [
+                    {
+                        "mountId": 25,
+                        "mountPoint": "/",
+                        "options": "rw,relatime",
+                        "type": "ext4",
+                    },
+                    {
+                        "mountId": 26,
+                        "mountPoint": "/dev",
+                        "options": "rw,nosuid,noexec,relatime",
+                        "type": "devtmpfs",
+                    },
+                ],
+            }
+        },
+    },
+]
+
+
+def test_host_info_parser():
+    parser = HostInfoParser()
+    output = parser.parse(hostInfos)
+    assert len(output) == 2
+    table_hardware = output[0]
+    assert table_hardware["type"] == "table"
+    assert table_hardware["caption"] == "Host Hardware Information"
+    assert table_hardware["headers"] == [
+        "Host",
+        "CPU Family",
+        "CPU Cores",
+        "Memory",
+        "OS",
+        "NUMA Enabled",
+    ]
+    assert table_hardware["rows"][0] == [
+        "M-QTFH0WFXLG:30017",
+        "Apple M1 Max (x86_64) 2400 MHz",
+        "10c",
+        "32.00 GB",
+        "Mac OS X 24.6.0",
+        False,
+    ]
+    assert table_hardware["rows"][1] == [
+        "ip-172-31-22-196",
+        "Intel(R) Xeon(R) Platinum 8175M CPU @ 2.50GHz (x86_64) 3200.679 MHz",
+        "2c",
+        "7.55 GB",
+        "Ubuntu 22.04",
+        False,
+    ]
+    table_mounts = output[1]
+    assert table_mounts["type"] == "table"
+    assert table_mounts["caption"] == "Mount Points"
+    assert table_mounts["headers"] == [
+        "Host",
+        "Mount Point",
+        "Type",
+        "Options",
+    ]
+    assert table_mounts["rows"][0] == [
+        "ip-172-31-22-196",
+        "/",
+        "ext4",
+        "rw,relatime",
+    ]
+    assert table_mounts["rows"][1] == [
+        "ip-172-31-22-196",
+        "/dev",
+        "devtmpfs",
+        "rw,nosuid,noexec,relatime",
+    ]
