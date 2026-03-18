4662724302e1c6c95c677c4097a57ea3443f0cd0
Yaoxing <yaoxing.zhang@gmail.com>
2026-03-18 13:53:02 +0100

    - add unit test for security parser.

--- src/x_ray/healthcheck/rules/security_rule.py
+++ src/x_ray/healthcheck/rules/security_rule.py
@@ -2,22 +2,22 @@
 Copyright (c) 2025 MongoDB Inc.
 
 DISCLAIMER: THESE CODE SAMPLES ARE PROVIDED FOR EDUCATIONAL AND ILLUSTRATIVE PURPOSES ONLY,
-TO DEMONSTRATE THE FUNCTIONALITY OF SPECIFIC MONGODB FEATURES. 
+TO DEMONSTRATE THE FUNCTIONALITY OF SPECIFIC MONGODB FEATURES.
 THEY ARE NOT PRODUCTION-READY AND MAY LACK THE SECURITY HARDENING, ERROR HANDLING, AND TESTING REQUIRED FOR A LIVE ENVIRONMENT.
-YOU ARE RESPONSIBLE FOR TESTING, VALIDATING, AND SECURING THIS CODE WITHIN YOUR OWN ENVIRONMENT BEFORE IMPLEMENTATION. 
+YOU ARE RESPONSIBLE FOR TESTING, VALIDATING, AND SECURING THIS CODE WITHIN YOUR OWN ENVIRONMENT BEFORE IMPLEMENTATION.
 THIS MATERIAL IS PROVIDED "AS IS" WITHOUT WARRANTY OR LIABILITY.
 """
+
 from x_ray.healthcheck.rules.base_rule import BaseRule
 from x_ray.healthcheck.issues import ISSUE, create_issue
 
 
 class SecurityRule(BaseRule):
-    def apply(self, data: object, **kwargs) -> tuple:
+    def apply(self, data: dict, **kwargs) -> tuple:
         """Check the security settings for any issues.
 
         Args:
-            data (object): The getCmdLineOpts data.
-            extra_info (dict, optional): Extra information such as host. Defaults to None.
+            data (dict): The getCmdLineOpts data.
         Returns:
             tuple: (list of issues found, list of parsed data)
         """
