0023cd786f7782dbc955b65729e7fcefab70afd9
Yaoxing Zhang <yaoxing.zhang@gmail.com>
2026-08-04 15:17:15 +0200

    change issue title.

--- src/x_ray/healthcheck/issues.py
+++ src/x_ray/healthcheck/issues.py
@@ -171,7 +171,7 @@ ISSUE_MSG_MAP = {
     ISSUE.ARBITER_MEMBER: {
         "id": ISSUE.ARBITER_MEMBER,
         "severity": SEVERITY.HIGH,
-        "title": "PSA Architecture",
+        "title": "Arbiters / PSA Architecture",
         "description": "`{set_name}` member `{host}` is an arbiter. Arbiters and PSA architecture are generally not recommended.",
     },
     ISSUE.IRRESPONSIVE_MONGOS: {
